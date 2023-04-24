33d742e5263e7787e1adc36e54d22cdf22ec5858
KevinFGR <kevinfelipe.gr@gmail.com>
2023-04-24 00:17:18 -0300

    error in repeated.py checked

--- auxiliares/repeated.py
+++ auxiliares/repeated.py
@@ -2,7 +2,7 @@
 
 import os
 
-folder = os.listdir("../arquivos/")
+folder = os.listdir("../arquivos")
 emails = {}
 for file in folder:
     with open(f'../arquivos/{file}', 'r', encoding='UTF-8') as arq:
@@ -10,7 +10,14 @@ for file in folder:
         for lin in linhas:
             if "Email :" in lin:
                 mail = lin.split(':')[1].strip()
-                for x in emails:
-                    if mail in emails[x]:
-                        print(f"the file {x} and {file} has the same email ({mail})")
-                emails[file] = mail
+            if "Assunto a ser colocado no email :" in lin:
+                assunto = lin.split(':')[1].strip()
+        if assunto == None: assunto = 0
+        for x in emails:
+            if mail in emails[x][0]:
+                print(f"the file {x} and {file} has the same email ({mail})")
+                if assunto != 0 and assunto in emails[x][1]:
+                    print(f'\tAnd the same subject: "{assunto}"')
+               
+        emails[file] = [mail,assunto]
+print('All files checked')
